c275a89151715d9503b87bd92c0a33e4ca7c2ab2
Boris Quiroz <boris@insert-coin.org>
2020-05-07 19:38:59 -0400

    fix int value

--- check.py
+++ check.py
@@ -5,9 +5,9 @@ def get_price():
     url = 'https://api.coindesk.com/v1/bpi/currentprice/CNY.json'
     r = requests.get(url)
     response = r.json()
-    value = response['bpi']['USD']['rate'][:5]
+    value = response['bpi']['USD']['rate_float']
 
-    print("BTC ${}".format(value))
+    print("BTC ${}".format(int(value)))
 
 if __name__ == "__main__":
     get_price()
