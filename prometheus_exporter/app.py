11ab64ef56bdf1649f4240aa20373b798c63cb14
Boris Quiroz <boris@insert-coin.org>
2022-06-01 10:28:16 -0400

    Remove USD price and add new sources for BTC price

--- prometheus_exporter/app.py
+++ prometheus_exporter/app.py
@@ -2,25 +2,24 @@ from prometheus_client import start_http_server, Gauge
 import requests
 import json
 
-btc_price = Gauge('btc_coindesk_price', 'Current BTC price from Coindesk')
-usd_clp = Gauge('usd_clp_price', 'Current USD/CLP convertion')
+btc_coindesk_price = Gauge('btc_coindesk_price', 'Current BTC price from Coindesk')
+btc_blockchain_price = Gauge('btc_blockchain_price', "Current BTC price from Blockchain.com")
 
 def get_prices():
     # BTC
-    btc_url = 'https://api.coindesk.com/v1/bpi/currentprice/CNY.json'
-    r = requests.get(btc_url)
-    response = r.json()
-    value = response['bpi']['USD']['rate_float']
+    btc_coindesk_url = 'https://api.coindesk.com/v1/bpi/currentprice/CNY.json'
+    coindesk_r = requests.get(btc_coindesk_url)
+    coindesk_response = coindesk_r.json()
+    coindesk_value = coindesk_response['bpi']['USD']['rate_float']
 
-    # USD
-    usd_url = "https://api.apilayer.com/fixer/latest?base=USD&symbols=CLP"
-    usd_headers = {'apikey': 'API KEY'}
-    u = requests.get(usd_url, headers=usd_headers)
-    u_response = u.json()
-    u_value = u_response['rates']['CLP']
+    btc_blockchain_url = 'https://api.blockchain.com/v3/exchange/tickers/BTC-USD'
+    blockchain_r = requests.get(btc_blockchain_url)
+    blockchain_response = blockchain_r.json()
+    blockchain_value = blockchain_response['last_trade_price']
 
-    btc_price.set(value)
-    usd_clp.set(u_value)
+    # Set gauges
+    btc_coindesk_price.set(coindesk_value)
+    btc_blockchain_price.set(blockchain_value)
 
 
 if __name__ == "__main__":
