63dc3cab8237afe7e0823a9c43d8c84a901a25f3
Boris Quiroz <borisquiroz@pm.me>
2024-04-13 23:21:18 -0400

    Add class to connect to RPC

---
+++ rpc/auth.py
@@ -0,0 +1,18 @@
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+class BitcoinRPC():
+    def __init__(self, rpc_user, rpc_password):
+        self.rpc_user = rpc_user
+        self.rpc_password = rpc_password
+        self.rpc_connection = AuthServiceProxy(f"http://{self.rpc_user}:{self.rpc_password}@127.0.0.1:8332",
+                                               timeout=10)
+
+
+    def getblockcount(self):
+        msg = f"Current block count: {self.rpc_connection.getblockcount()}"
+        return msg
+
+    def getchaintips(self):
+        return self.rpc_connection.getchaintips()
+
+
